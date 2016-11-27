354a463adff9f5b943941bdb53ce844c55856fe0
Matt Sybeldon <mattsybeldon@gmail.com>
2016-11-27 12:42:14 -0500

    Most of the code for reading the MIDI file is there, but the short message is currently failing.

--- Tab-Generator/src/music_readers/MidiReader.java
+++ Tab-Generator/src/music_readers/MidiReader.java
@@ -8,6 +8,7 @@ import javax.sound.midi.Sequence;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,13 +16,70 @@ public class MidiReader {
 	
 	Logger logger = LoggerFactory.getLogger(MidiReader.class);
 	
-	public static final int note_on = 0x90; //Note onset
-	public static final int note_off = 0x80; //Note offset
+	public static final int noteOn = 0x90; //Note onset
+	public static final int noteOff= 0x80; //Note offset
 	public static final String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-	public String filePath; //Path to the MIDI file
-	Sequence sequence = MidiSystem.getSequence(new File(filePath));
+	
+	
+	private Sequence midiSequence;
+	private Track[] allTracks;
+	
 	int numTracks;
 	int[] trackPositions = new int[numTracks];
 	
-
+	
+	public MidiReader(String filePath){
+		try {
+			midiSequence = MidiSystem.getSequence(new File(filePath));
+		} catch(Exception midiIOError){
+			midiIOError.printStackTrace();
+			System.out.println("Could not load MIDI file supplied.");
+		}
+		
+		//Extract tracks at initialization
+		allTracks = midiSequence.getTracks();
+		numTracks = allTracks.length;
+	}
+	
+	//Used to check contents of a track for debugging and testing
+	public void enumerateTrack(int trackNumber){
+		
+		Track currentTrack = allTracks[trackNumber];
+		/*MIDI files are composed of events which capture information such as note
+		 * onset, offset, and the velocity of the  note. The velocity is not needed
+		 * for our purposes, so we will not read it. Go thru all events*/
+		for (int i = 0; i < currentTrack.size(); i++){
+			
+			MidiEvent event = currentTrack.get(i);
+			MidiMessage message = event.getMessage();
+			
+			if (message instanceof ShortMessage){  //This check is failing
+				ShortMessage currentShort = (ShortMessage) message;
+				
+				
+				if (currentShort.getCommand() == noteOn){
+					
+					int key = currentShort.getData1();
+					int octave = (key/12) - 1;
+					int note = key % 12;
+					String noteName = notes[note];
+					System.out.println("Note on: " + noteName + octave + "key: " + key);
+					
+				}else if (currentShort.getCommand() == noteOff){
+					
+					int key = currentShort.getData1();
+					int octave = (key/12) - 1;
+					int note = key % 12;
+					String noteName = notes[note];
+					System.out.println("Note off: " + noteName + octave + "key: " + key);
+					
+				}
+			}
+			
+		}
+		
+		
+	}
+		
+	
 }
